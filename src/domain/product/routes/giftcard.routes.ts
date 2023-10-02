ec5e5e6cc5f806495c6457773312e1f89ec2874d
Thalison Eduardo <thalison.eduardo14@gmail.com>
2023-10-02 19:44:46 -0300

    refactor: change giftcard seed routes

--- src/domain/product/routes/giftcard.routes.ts
+++ src/domain/product/routes/giftcard.routes.ts
@@ -2,5 +2,5 @@ import { RouterHelper } from '@/main/common'
 import { ListGiftCardController } from '../controller/ListGiftCardController'
 
 export const giftCardRoutes = RouterHelper.route(route => {
-    route.get('/gitcards', RouterHelper.adapter(ListGiftCardController))
+    route.get('/giftcards', RouterHelper.adapter(ListGiftCardController))
 })
--- src/main/database/seeding/GiftCard.seed.ts
+++ src/main/database/seeding/GiftCard.seed.ts
@@ -12,42 +12,42 @@ export default class CreateGiftCards implements Seeder {
             .values([
                 {
                     name: 'Spotify',
-                    logo: 'https://www.grupocard.com.br/image/produtos/streaming/spotify.jpg',
+                    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Spotify_icon.svg/232px-Spotify_icon.svg.png',
                     category: GiftCardCategory.audiovisual
                 },
                 {
                     name: 'Google',
-                    logo: 'https://www.grupocard.com.br/image/produtos/conteudo/play-store.png',
+                    logo: 'https://developer.android.com/static/images/cards/newsletter-play-july-2023.png?hl=pt-br',
                     category: GiftCardCategory.store
                 },
                 {
                     name: 'Apple',
-                    logo: 'https://www.grupocard.com.br/image/produtos/conteudo/apple-store.png',
+                    logo: 'https://assets.simon.com/tenantlogos/279.png',
                     category: GiftCardCategory.store
                 },
                 {
                     name: 'Netflix',
-                    logo: 'https://www.grupocard.com.br/image/produtos/streaming/netflix.jpg',
+                    logo: 'https://cdn-icons-png.flaticon.com/512/732/732228.png',
                     category: GiftCardCategory.audiovisual
                 },
                 {
                     name: 'Playstation',
-                    logo: 'https://www.grupocard.com.br/image/produtos/jogos/playstation.jpg',
+                    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/PlayStation_logo.svg/2560px-PlayStation_logo.svg.png',
                     category: GiftCardCategory.games
                 },
                 {
                     name: 'Xbox',
-                    logo: 'https://www.grupocard.com.br/image/produtos/jogos/xboxgamepass.jpg',
+                    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Xbox_one_logo.svg/2048px-Xbox_one_logo.svg.png',
                     category: GiftCardCategory.games
                 },
                 {
                     name: 'Minecraft',
-                    logo: 'https://www.grupocard.com.br/image/produtos/jogos/minecraft.jpg',
+                    logo: 'https://res.cloudinary.com/zenbusiness/image/upload/v1670445040/logaster/logaster-2020-06-image14-3.png',
                     category: GiftCardCategory.games
                 },
                 {
                     name: 'Office',
-                    logo: 'https://www.grupocard.com.br/image/produtos/conteudo/office.png',
+                    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Microsoft_Office_2013-2019_logo_and_wordmark.svg/2560px-Microsoft_Office_2013-2019_logo_and_wordmark.svg.png',
                     category: GiftCardCategory.software
                 },
                 {
@@ -57,17 +57,17 @@ export default class CreateGiftCards implements Seeder {
                 },
                 {
                     name: 'Uber',
-                    logo: 'https://www.grupocard.com.br/image/produtos/transporte/uber.jpg',
+                    logo: 'https://logospng.org/download/uber/logo-uber-2048.png',
                     category: GiftCardCategory.services
                 },
                 {
                     name: 'Bilhete único',
-                    logo: 'https://www.grupocard.com.br/image/produtos/transporte/bilhete-unico.png',
+                    logo: 'https://bu.voud.com.br/assets/images/bom/logo_bu.png',
                     category: GiftCardCategory.services
                 },
                 {
                     name: 'Telesena',
-                    logo: 'https://www.grupocard.com.br/image/produtos/capitalizacao/telesena.jpgas',
+                    logo: 'https://devplace.com.br/wp-content/uploads/2016/05/logo-telesena-210x210.png',
                     category: GiftCardCategory.services
                 }
             ])
